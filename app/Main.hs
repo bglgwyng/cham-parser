e10ef97deb8edbc26c23810283f7bd738c2671de
bglgwyng <bglgwyng@gmail.com>
2019-12-10 23:57:49 +0900

    Emit JSON

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
+import Data.Aeson
 import Text.Megaparsec
+import Prelude hiding (putStrLn)
+import Data.String.Class (putStrLn)
 
 import Lib
 
 main :: IO ()
 main = do
   input <- getContents
-  parseTest source input
+  putStrLn $ show options
+  case parse source "" input of
+    Left error -> putStrLn $ errorBundlePretty error
+    Right ast -> putStrLn $ encode ast
   return ()
--- src/AST.hs
+++ src/AST.hs
@@ -1,13 +1,20 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE NamedFieldPuns       #-}
+{-# LANGUAGE OverloadedStrings    #-}
 
 module AST where
 
+import           Data.Aeson
+-- import           Data.Vector
 import           Data.List
+import           GHC.Generics hiding (Constructor)
 
 data Annotation =
     SimpleAnnotation String |
     AssignmentAnnotation String String
+    deriving Generic
 
 instance Show Annotation where
     show (SimpleAnnotation x)       = "@" ++ x
@@ -21,6 +28,7 @@ instance {-# Overlapping #-} Show Annotations where
 data Argument =
     NamedArgument String Term |
     UnnamedArgument Term
+    deriving Generic
 
 instance Show Argument where
     show (NamedArgument x y) = "(" ++ x ++ " : " ++ show y ++ ")"
@@ -31,24 +39,30 @@ data Term =
     Apply Term Term |
     Parenthesized Term |
     Variable String
+    deriving Generic
 
 instance Show Term where
     show x = case x of
-        Arrow y z  -> show y ++ " -> " ++ show z
-        Apply y z  -> show y ++ " " ++ show z
-        Parenthesized y    -> "(" ++ show y ++ ")"
-        Variable y -> y
+        Arrow y z       -> show y ++ " -> " ++ show z
+        Apply y z       -> show y ++ " " ++ show z
+        Parenthesized y -> "(" ++ show y ++ ")"
+        Variable y      -> y
 
 data Constructor =
     Simple [Term] |
     Record [(String, Term)]
+    deriving Generic
 
 instance Show Constructor where
     show (Simple as) = concat (map ((" " ++) . show) as)
     show (Record fields) = " { " ++ intercalate ", " [x ++ " : " ++ show y | (x, y) <- fields] ++ " }"
 
 -- TODO: Better name
-data ImportRule = Unqualifed | UnqualifedOnly [(String, ImportRule)] | Qualified String
+data ImportRule =
+    Unqualifed |
+    UnqualifedOnly [(String, ImportRule)] |
+    Qualified String
+    deriving Generic
 
 instance Show ImportRule where
     show Unqualifed = "";
@@ -72,6 +86,8 @@ data TopLevelDeclaration =
         rule        :: ImportRule,
         annotations :: Annotations
     }
+    deriving Generic
+
 
 instance Show TopLevelDeclaration where
     show DataDeclaration { name, args, variants, annotations } =
@@ -87,7 +103,31 @@ instance Show TopLevelDeclaration where
         show annotations
         ++ "import " ++ show url ++ show rule
 
-data Source = Source [TopLevelDeclaration]
+data Source = Source [TopLevelDeclaration] deriving Generic
 
 instance Show Source where
     show (Source definitions) = intercalate "\n" (map show definitions)
+
+options :: Options    
+options = defaultOptions { sumEncoding = TwoElemArray }
+
+instance ToJSON Annotation where
+    toEncoding = genericToEncoding options
+    
+instance ToJSON Argument where
+    toEncoding = genericToEncoding options
+        
+instance ToJSON Term where
+    toEncoding = genericToEncoding options
+
+instance ToJSON Constructor where
+    toEncoding = genericToEncoding options
+
+instance ToJSON ImportRule where
+    toEncoding = genericToEncoding options
+
+instance ToJSON TopLevelDeclaration where
+    toEncoding = genericToEncoding options
+
+instance ToJSON Source where
+    toEncoding = genericToEncoding options
